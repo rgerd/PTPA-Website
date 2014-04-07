f42e0761c93325ba9aa66f35cc134c7480a2cdeb
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-07 09:18:49 -0400

    Created index
    Created an index.php file that says hello to world.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hello World</title>
+	</head>
+
+	<body>
+		<div id="hello_world_div">
+			<?php
+				$message = "Hello world!";
+				$tag = "h1";
+				echo "<".$tag.">".$message."</".$tag.">";
+			?>
+		</div>
+	</body>
+</html>
