dd93e8cd552a08568bd83cd4b283bcf496d8ecd9
laurelfink <laurelfink12197@gmail.com>
2014-04-15 09:38:05 -0400

    More Database Functions

---
+++ model/database.php
@@ -0,0 +1,4 @@
+<?php
+//to connect to database.
+//not exactly sure how to make this work in github......
+?>
--- model/mysql.php
+++ model/mysql.php
@@ -52,6 +52,62 @@ function count_signups($task){
     return $results;
 }
 
+function add_event($accountID, $date, $title, $desc){
+    global $db;
+    $query = "INSERT INTO events (accountID, date, title, desc) VALUES ('$accountID', '$date', '$title', '$desc')";
+    $results = $db->execute($query);
+    return $results;
+}
+
+function add_task($eventID, $desc, $count){
+    global $db;
+    $query = "INSERT INTO tasks (eventID, desc, count) VALUES ('$eventID', '$desc', '$count')";
+    $results = $db->execute($query);
+    return $results;
+}
+
+function signup($taskID, $accountID, $comment = null){
+    global $db;
+    $query = "INSERT INTO signups (taskID, accountID, comment) VALUES ('$taskID', '$accountID', '$comment')";
+    $results = $db->execute($query);
+    return $results;
+}
+
+function edit_signup($id, $comment){
+    global $db;
+    $query = "UPDATE signups SET comment='$comment' WHERE ID='$id'";
+    $results = $db->execute($query);
+    return $results;
+}
+
+function delete_signup($id){
+    global $db;
+    $query = "DELETE FROM signups WHERE ID = '$id'";
+    $results = $db->execute($query);
+    return $results;
+}
+
+function edit_account($id, $fname, $lname, $email, $phone, $pass){
+    global $db;
+    $query = "UPDATE accounts SET fname='$fname',lname='$lname',email='$email',phone='$phone',password='$pass' WHERE ID='$id'";
+    $results = $db->execute($query);
+    return $results;
+}
+
+function delete_account($id){
+    global $db;
+    $query = "DELETE FROM accounts WHERE ID = '$id'";
+    $results = $db->execute($query);
+    return $results;
+}
+
+function delete_event($id){
+    global $db;
+    $query = "DELETE FROM events WHERE ID = '$id'";
+    $results = $db->execute($query);
+    return $results;
+}
+
 
 ?>
 
