4d0ea0687f48fb56a810cc87956ebeadb68028b5
laurelfink <laurelfink12197@gmail.com>
2014-04-16 12:57:43 -0400

    Header Works Now!!!!!

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php $page_title = "Event!"; include 'view/header.php'; ?>
-		<div id="hello_world_div">
-			<?php
-				$message = "Hello world!!";
-				$tag = "h1";
-                echo "<".$tag.">".$message."</".$tag.">";
-			?>
-		</div>
+<!--		<div id="hello_world_div">-->
+<!--			--><?php
+//				$message = "Hello world!!";
+//				$tag = "h1";
+//                echo "<".$tag.">".$message."</".$tag.">";
+//			?>
+<!--		</div>-->
 <?php include 'view/event.php'; ?>
 
 <?php include 'view/footer.php'; ?>
--- view/header.php
+++ view/header.php
@@ -6,11 +6,12 @@
     <link rel="stylesheet" type="text/css" href="css/event_style.css"/>
 </head>
 <body>
-<div id="nav_bar">
-    <ul>
-        <li><a href="index.php"><img src="css/images/logo1.png" alt="PT Volunteer"/></a></li>
-        <li><a href=""><img src="css/images/gear.png" alt=""/></a></li>
-        <li><a href=""><img src="css/images/signout.png" alt=""/></a></li>
-    </ul>
+<div >
+    <div id="nav_bar">
+            <div id="float_left"><a href="index.php"><img src="css/images/logo1.png" alt="PT Volunteer" height="60px"/></a></div>
+            <div id="float_right"><a href=""><img src="css/images/gear.png" alt="" height="50px"/></a>
+            <a href=""><img src="css/images/signout.png" alt="" height="50px" /></a></div>
+
+    </div>
 <div/>
 
