f675f7113a18c68e046b86f88cd6b8e0966809f4
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-16 01:12:36 -0400

    Cleaned up event page

--- view/event.php
+++ view/event.php
@@ -1,12 +1,5 @@
 <?php
-	$page_title = "Event!";
-	include "header.php"
-	//require "event_task.php"
-
-
-	//echoEventTask("Event Task 1", 4, 5, "");
-
-
-	include "footer.php";
+	include "event_task.php";
+	echoEventTask("Event Task 1", 4, 5, "");
+	echoEventTask("Event Task 2", 2, 5, "");
 ?>
-HELLO
