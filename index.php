a5995267d76a2fd5fb150486cc0098ddf818bce8
laurelfink <laurelfink12197@gmail.com>
2014-04-16 10:35:33 -0400

    Nav Bar Addition

--- index.php
+++ index.php
@@ -4,10 +4,8 @@
 				$message = "Hello world!!";
 				$tag = "h1";
                 echo "<".$tag.">".$message."</".$tag.">";
-                echo "MOO";
-                echo "This my change."
 			?>
 		</div>
-<!--<?php include 'view/event.php'; ?>-->
+<?php include 'view/event.php'; ?>
 
 <?php include 'view/footer.php'; ?>
--- view/header.php
+++ view/header.php
@@ -6,4 +6,10 @@
     <link rel="stylesheet" type="text/css" href="css/event_style.css"/>
 </head>
 <body>
+<div id="nav_bar">
+<li>PTPA Website</li>
+<li id="nav_bar_left"><img src="css/images/gear.png" alt="Settings" height="50px" width="50px"/></li>
+<li id="nav_bar_left"><img src="css/images/signout.png" alt="Sign-Out" height="50px" width="50px"/></li>
+<div/>
+
 <p>THIS IS THE HEADER</p>
