1fb5b6cafcf77ffaead52ef58eb89082426ea07f
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-16 14:51:31 -0400

    Made event page more dynamic

--- view/event.php
+++ view/event.php
@@ -1,5 +1,32 @@
-<?php
-	include "event_task.php";
-	echoEventTask("Event Task 1", 4, 5, "");
-	echoEventTask("Event Task 2", 2, 5, "");
-?>
+<div id="event_tasks">
+	<?php for($i = 0; $i < 10; $i++): ?>
+	<div class="event_task">
+		<div class="event_task_header">
+			<div class="event_task_title"><?php echo "Event Task ".($i + 1); ?></div>
+			<div class="event_task_expander" expanded="false"></div>
+		</div>
+		<br />
+		<div class="event_task_details" style="display: none;">
+			<div class="event_signup_info">
+				<div class="slots_full">
+					<?php echo /*"$slots_full/$total_slots"*/ "4/5"; ?> Slots Full
+				</div>
+				<div class="signed_up">
+					Already Signed Up:
+					<div class="sign_up_list">
+					<ul>
+						<li>Person: comment</li>
+						<li>Person: comment</li>
+						<li>Person: comment</li>
+						<li>Person: comment</li>
+						<li>Person: comment</li>
+						<li>Person: comment</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="event_sign_up_button">Sign Up</div>
+		</div>
+	</div>
+	<?php endfor; ?>
+</div>
