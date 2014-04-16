68bf870bc4c06c49e5eb2038ac9441a0e5d3aa2f
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-16 10:26:44 -0400

    Formatted header.php

--- view/header.php
+++ view/header.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
-    <html>
+<html>
 <head>
-    <title>
-        <?php echo $page_title; ?>
-    </title>
+    <title><?php echo $page_title; ?></title>
     <link rel="stylesheet" type="text/css" href="css/style.css"/>
     <link rel="stylesheet" type="text/css" href="css/event_style.css"/>
 </head>
-
 <body>
 <p>THIS IS THE HEADER</p>
