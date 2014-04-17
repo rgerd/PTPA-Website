ab8fbe1fe4918300cb0eaed073ec27516db7a423
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-16 20:23:35 -0400

    Made index more dynamic

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-	$page_title = "Event!";
-	$page = "view/event.php";
+	
+	if(isset($_GET['event_id'])) {
+		$event_id = $_GET['event_id'];
+		$page_title = "Event!";
+		$page = "view/event.php";
+	}
 ?>
 <?php include 'view/header.php'; ?>
 <?php include $page; ?>
