fb90045a60e6318e0a888015b25b2a7dd663b46e
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-16 21:47:02 -0400

    Formatted database code

--- model/database.php
+++ model/database.php
@@ -5,8 +5,6 @@ $db_user = "root";
 $db_pass = "root";
 try {
 	$db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
-
-
 } catch(PDOException $e) {
 	echo "PDO ERROR: ".($e ->getMessage())."<br />";
 	die();
