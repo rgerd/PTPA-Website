398e41ad52ed27b66e80245710e9e3439d80ef29
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-17 13:37:01 -0400

    Enhanced footer

--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,6 @@
 
 </div>
 </div>
-<marquee behavior="scroll" direction="right" scrollamount="10" style="color: #F0F"><p>THIS IS THE FOOTER</p></marquee>
+<marquee behavior="scroll" direction="right" scrollamount="10" style="color: #F0F; margin-top: 500px"><p>THIS IS THE FOOTER</p></marquee>
 </body>
 </html>
