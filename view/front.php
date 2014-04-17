040364350087d0326d25594ee32d57f069a83a98
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-17 09:55:20 -0400

    Working on front page form

--- view/front.php
+++ view/front.php
@@ -5,9 +5,10 @@
 	</ul>
 
 	<div id="sign_in" class="tab_content">
-		<form id="sign_in_form">
-			<input type="text" placeholder="Email"/>
-			<input type="password" placeholder="Password"/>
+		<form id="sign_in_form" method="POST" action=".">
+			<input type="text" spellcheck = "false" autocorrect = "false" placeholder="Email"/>
+			<input type="password" spellcheck = "false" autocorrect = "false" placeholder="Password"/>
+			<input type="submit" spellcheck = "false" autocorrect = "false"  value="Sign In!"/>
 		</form>
 	</div>
 	<div id="sign_up" class="tab_content">
