b1758a0a1dcb50d3f924505ea3d9d11cde5ce3b7
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-18 14:10:04 -0400

    Adding cookies

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
+	session_start();
 	include "model/database.php";
 	include "model/mysql.php";
+	include "model/cookie.php";
+
+	
+
+	if(isset($_SESSION['USER_ID'])) {
+		$user_id = $_SESSION['USER_ID'];
+		$page = "view/home.php";
+	}
+
 
 	$action = $_POST['action'];
 	$fname = $_POST['fname'];
 	$lname = $_POST['lname'];
 	$email = $_POST['email'];
 	$entered_password = !(!$_POST['password']);
-	echo $entered_password;
 	$password = md5($_POST['password']); // encrypted for security
 	$pnum = $_POST['pnum'];
 
@@ -15,18 +24,31 @@
 		$top_tab = "sign_in";
 		if(!$email || !$entered_password) {
 			$sign_in_error_message = "Please fill in all fields!";
-		} else if(!auth_user($email, $password)) {
-			$sign_in_error_message = "Incorrect email or password!";
+		} else {
+			$user_id = auth_user($email, $password);
+			if($user_id == -1) {
+				$sign_in_error_message = "Incorrect username or password!";
+			} else {
+				$_SESSION['USER_ID'] = $user_id;
+				$page = "view/home.php";
+			}
 		}
 	} else if($action == "sign_up") {
 		$top_tab =  "sign_up";
+		if(!$fname || !$lname || !$email || !$entered_password || !$pnum) {
+			$sign_up_error_message = "Please fill in all fields!";
+		} else {
+			//register_user($fname, $lname, $email, $pnum, $password);
+		}
 	}
 
 	if(isset($_GET['event_id'])) {
 		$event_id = $_GET['event_id'];
 		$page_title = "Event!";
 		$page = "view/event.php";
-	} else {
+	}
+
+	if(!$page) {
 		$page = "view/front.php";
 	}
 ?>
