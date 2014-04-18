332d6a2d4ae72325d264e3aedd26eac1607c0b75
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-18 10:55:04 -0400

    Added error checking to registration

--- index.php
+++ index.php
@@ -6,12 +6,16 @@
 	$fname = $_POST['fname'];
 	$lname = $_POST['lname'];
 	$email = $_POST['email'];
+	$entered_password = !(!$_POST['password']);
+	echo $entered_password;
 	$password = md5($_POST['password']); // encrypted for security
 	$pnum = $_POST['pnum'];
 
 	if($action == "sign_in") {
-		if(!auth_user($email, $password)) {
-			$top_tab = "sign_in";
+		$top_tab = "sign_in";
+		if(!$email || !$entered_password) {
+			$sign_in_error_message = "Please fill in all fields!";
+		} else if(!auth_user($email, $password)) {
 			$sign_in_error_message = "Incorrect email or password!";
 		}
 	} else if($action == "sign_up") {
