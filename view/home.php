eb7abc9199297237cb7fff06cb7b458b90a8c068
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-21 14:18:33 -0400

    Added home page

--- view/home.php
+++ view/home.php
@@ -1,10 +1,27 @@
 <div id="home">
 	<div id="top">
-		<span style="float:left;">Events</span>
-		<span style="float:right;"><a class="link_button" href="#">Create New Event</a></span>
+		<div style="float:left; display: block;">Events</div>
+		<div style="float:right; display: block;"><a class="link_button" href="#">Create New Event</a></div>
 	</div>
-
+	<br />
+	<br />
 	<div id="events_table">
-		
+		<div class="event">
+			<div class="event_title">Event 1</div>
+			<div class="event_description">This is an event. This is an event. This is an event. This is an event. This is an event. This is an event. This is an event.</div>
+			<div class="event_share"><a class="link_button">Share</a></div>
+		</div>
+
+		<div class="event">
+			<div class="event_title">Event 2</div>
+			<div class="event_description">This is an event. This is an event. This is an event. This is an event. This is an event. This is an event. This is an event.</div>
+			<div class="event_share"><a class="link_button">Share</a></div>
+		</div>
+
+		<div class="event">
+			<div class="event_title">Event 3</div>
+			<div class="event_description">This is an event. This is an event. This is an event. This is an event. This is an event. This is an event. This is an event.</div>
+			<div class="event_share"><a class="link_button">Share</a></div>
+		</div>
 	</div>
 </div>
