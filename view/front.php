df3908a27f4ee4f636f3b27bbf344b2b32c09cfe
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-22 15:07:26 -0400

    Added new selector to front form elements

--- view/front.php
+++ view/front.php
@@ -7,8 +7,8 @@
 
 	<div id="sign_in" class="tab_content">
 		<form id="sign_in_form" method="POST" action=".">
-			<input class="front_input" type="text" name="email" value="<?php if($action == "sign_in") echo $email; ?>" spellcheck = "false" autocorrect = "false" placeholder="Email"/>
-			<input class="front_input" type="password" name="password" spellcheck = "false" autocorrect = "false" placeholder="Password"/>
+			<input class="front_input form_text_field" type="text" name="email" value="<?php if($action == "sign_in") echo $email; ?>" spellcheck = "false" autocorrect = "false" placeholder="Email"/>
+			<input class="front_input form_text_field" type="password" name="password" spellcheck = "false" autocorrect = "false" placeholder="Password"/>
 			<span style="background-color: #E0E0E0;"><input class="front_input _3D" type="submit" spellcheck = "false" autocorrect = "false"  value="Sign In!"/></span>
 			<?php if($sign_in_error_message) echo '<div class="error_message">'.$sign_in_error_message.'</div>'; ?>
 			<input name="action" type="hidden" value="sign_in"/>
@@ -16,11 +16,11 @@
 	</div>
 	<div id="sign_up" class="tab_content">
 		<form id="sign_up_form" method="POST" action=".">
-			<input class="front_input" type="text" name="fname" value="<?php if($action == "sign_up") echo $fname; ?>" spellcheck = "false" autocorrect = "false" placeholder="First Name"/>
-			<input class="front_input" type="text" name="lname" value="<?php if($action == "sign_up") echo $lname; ?>" spellcheck = "false" autocorrect = "false" placeholder="Last Name"/>
-			<input class="front_input" type="text" name="email" value="<?php if($action == "sign_up") echo $email; ?>" spellcheck = "false" autocorrect = "false" placeholder="Email"/>
-			<input class="front_input" type="text" name="pnum" value="<?php if($action == "sign_up") echo $pnum; ?>" spellcheck = "false" autocorrect = "false" placeholder="Phone Number"/>
-			<input class="front_input" type="password" name="password" spellcheck = "false" autocorrect = "false" placeholder="Password"/>
+			<input class="front_input form_text_field" type="text" name="fname" value="<?php if($action == "sign_up") echo $fname; ?>" spellcheck = "false" autocorrect = "false" placeholder="First Name"/>
+			<input class="front_input form_text_field" type="text" name="lname" value="<?php if($action == "sign_up") echo $lname; ?>" spellcheck = "false" autocorrect = "false" placeholder="Last Name"/>
+			<input class="front_input form_text_field" type="text" name="email" value="<?php if($action == "sign_up") echo $email; ?>" spellcheck = "false" autocorrect = "false" placeholder="Email"/>
+			<input class="front_input form_text_field" type="text" name="pnum" value="<?php if($action == "sign_up") echo $pnum; ?>" spellcheck = "false" autocorrect = "false" placeholder="Phone Number"/>
+			<input class="front_input form_text_field" type="password" name="password" spellcheck = "false" autocorrect = "false" placeholder="Password"/>
 			<span style="background-color: #E0E0E0;"><input class="front_input _3D" type="submit" spellcheck = "false" autocorrect = "false"  value="Sign Up!"/></span>
 			<?php if($sign_up_error_message) echo '<div class="error_message">'.$sign_up_error_message.'</div>'; ?>
 			<input name="action" type="hidden" value="sign_up"/>
