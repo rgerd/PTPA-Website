6973e002793b72944de3d4a6bf907479e59fb842
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-23 09:26:47 -0400

    Added file to handle registration

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+	session_start();
+	include "model/database.php";
+	include "model/mysql.php";
+	include "model/cookie.php";
+
+
+	$user_id = isset($_SESSION['USER_ID']) ? $_SESSION['USER_ID'] : -1;
+	if($user_id != -1)
+		$page = "view/home.php";
+
+	$action = isset($_POST['action']) ? $_POST['action'] : "none";
+
+	include "model/registration.php";
+
+	if(isset($_GET['e'])) {
+		$event_id = $_GET['e'];
+		$event = get_event($event_id);
+		$page_title = $event['title'];
+		$page = "view/event.php";
+	}
+
+	if($action == "task_sign_up") {
+		$event_id = $_POST['event'];
+		$task_id = $_POST['task'];
+
+		
+	}
+
+	$page = isset($page) ? $page : "view/front.php";
+
+	$page_title = "PT Volunteer";
+?>
+<?php include 'view/header.php'; ?>
+<?php include "$page"; ?>
+<?php include 'view/footer.php'; ?>
