0904d6e57e96f1c53d92d53c65ea85cd73847a9d
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-24 10:36:36 -0400

    Added cookies

--- model/registration.php
+++ model/registration.php
@@ -15,6 +15,7 @@ if($action == "sign_in") {
 		if($user_id == -1) {
 			$sign_in_error_message = "Incorrect username or password!";
 		} else {
+			set_cookie_for_user($user_id);
 			$_SESSION['USER_ID'] = $user_id;
 			$page = "view/home.php";
 		}
@@ -27,7 +28,7 @@ if($action == "sign_in") {
 		$sign_up_error_message = "This email is already registered!<br /><a href='.'>Forgot your password?</a>";
 	} else {
 		$user_id = register_user($fname, $lname, $email, $pnum, $password);
-		$set_cookie_for_user($user_id);
+		set_cookie_for_user($user_id);
 		$_SESSION['USER_ID'] = $user_id;
 		$page = "view/home.php";
 	}
