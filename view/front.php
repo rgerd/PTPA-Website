c801fab1e95a8b448d4d938dc8d77eb8ca0ece58
laurelfink <laurelfink12197@gmail.com>
2014-04-24 10:49:10 -0400

    NEW BUTTONS!!!

--- view/front.php
+++ view/front.php
@@ -10,7 +10,7 @@
 		<form id="sign_in_form" method="POST" action=".">
 			<input class="form_text_field" type="text" name="email" value="<?php if($action == "sign_in") echo $email; ?>" spellcheck = "false" autocorrect = "false" placeholder="Email"/>
 			<input class="form_text_field" type="password" name="password" spellcheck = "false" autocorrect = "false" placeholder="Password"/>
-			<span style="background-color: #E0E0E0;"><input class="footer_button _3D" type="submit" spellcheck = "false" autocorrect = "false"  value="Sign In!"/></span>
+			<span style="background-color: #E0E0E0;"><input class="css_button" type="submit" spellcheck = "false" autocorrect = "false"  value="Sign In!"/></span>
 			<?php $sign_in_error_message = isset($sign_in_error_message) ? $sign_in_error_message : false; ?>
             <?php if($sign_in_error_message) echo '<div class="error_message">'.$sign_in_error_message.'</div>'; ?>
 			<input name="action" type="hidden" value="sign_in"/>
@@ -23,7 +23,7 @@
 			<input class="form_text_field" type="text" name="email" value="<?php if($action == "sign_up") echo $email; ?>" spellcheck = "false" autocorrect = "false" placeholder="Email"/>
 			<input class="form_text_field" type="text" name="pnum" value="<?php if($action == "sign_up") echo $pnum; ?>" spellcheck = "false" autocorrect = "false" placeholder="Phone Number"/>
 			<input class="form_text_field" type="password" name="password" spellcheck = "false" autocorrect = "false" placeholder="Password"/>
-			<span style="background-color: #E0E0E0;"><input class="footer_button _3D" type="submit" spellcheck = "false" autocorrect = "false"  value="Sign Up!"/></span>
+			<span style="background-color: #E0E0E0;"><input class="css_button" type="submit" spellcheck = "false" autocorrect = "false"  value="Sign Up!"/></span>
             <?php if(isset($sign_up_error_message) && $sign_in_error_message) echo '<div class="error_message">'.$sign_up_error_message.'</div>'; ?>
 			<input name="action" type="hidden" value="sign_up"/>
 		</form>
