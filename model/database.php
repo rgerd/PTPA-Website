9307853495d9d298e65f35848f09440f3b2bb138
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-25 11:56:47 -0400

    Fixed SQL username bug

--- model/database.php
+++ model/database.php
@@ -3,10 +3,20 @@ $host = "localhost";
 $db_name = "PTPA_DB";
 $db_user = "root";
 $db_pass = "root";
-try {
-	$db = new PDO("mysql:host=$host;dbname=$db_name", $db_user, $db_pass);
-} catch(PDOException $e) {
-	echo "PDO ERROR: ".($e ->getMessage())."<br />";
-	die();
+
+
+if(!connect("root")) {
+	if(!connect("user")) {
+		die();
+	}
+}
+
+function connect($user) {
+	try {
+		$db = new PDO("mysql:host=$host;dbname=$db_name", $user, $db_pass);
+	} catch(PDOException $e) {
+		echo "PDO ERROR: ".($e ->getMessage())."<br />";
+		return false;
+	}
 }
 ?>
