39405bf65e3edff9037b029005c3cda31ff4fbba
laurelfink <laurelfink12197@gmail.com>
2014-04-25 12:39:32 -0400

    Event page buttons

--- view/home.php
+++ view/home.php
@@ -1,27 +1,28 @@
 <div id="home">
 	<div id="top">
 		<div style="float:left; display: block;">Events</div>
-		<div style="float:right; display: block;"><a class="link_button" href="#">Create New Event</a></div>
+		<div  style="float:right; display: block;"><a class="css_button_a" href="#"><div class="css_button_div">Create New Event</div></a></div>
 	</div>
+
 	<br />
 	<br />
 	<div id="events_table">
 		<div class="event">
 				<div class="event_title">Event 1</div>
 				<div class="event_description">This is an event. This is an event. This is an event. This is an event. This is an event. This is an event. This is an event.</div>
-				<div class="event_share"><a class="link_button">Share</a></div>
+				<div class="event_share"><a class="css_button_a"><div class="css_button_div_share">Share</div></a></div>
 		</div>
 
 		<div class="event">
 			<div class="event_title">Event 2</div>
 			<div class="event_description">This is an event. This is an event. This is an event. This is an event. This is an event. This is an event. This is an event.</div>
-			<div class="event_share"><a class="link_button">Share</a></div>
+			<div class="event_share"><a class="css_button_a"><div class="css_button_div_share">Share</div></a></div>
 		</div>
 
 		<div class="event">
 			<div class="event_title">Event 3</div>
 			<div class="event_description">This is an event. This is an event. This is an event. This is an event. This is an event. This is an event. This is an event.</div>
-			<div class="event_share"><a class="link_button">Share</a></div>
+			<div class="event_share"><a class="css_button_a"><div class="css_button_div_share">Share</div></a></div>
 		</div>
 	</div>
 </div>
