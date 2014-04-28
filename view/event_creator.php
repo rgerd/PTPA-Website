538d6223100b3391d5b0cd9cb03c5699bca93c53
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-28 14:05:21 -0400

    Adding event task slot

---
+++ view/event_creator.php
@@ -0,0 +1,11 @@
+<script type="text/javascript" src="js/event_creator.js"></script>
+
+<div class="event_task">
+	<div class="task_left">
+		<input type="text" />
+	</div>
+	<div class="task_right">
+		<input type="checkbox" />
+		<div class="delete_button">Delete</div>
+	</div>
+</div>
