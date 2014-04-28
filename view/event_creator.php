d38c030ece63e7619fc714c6ca04775bddecd2a2
Robert Gerdisch <rgerdisch@gmail.com>
2014-04-28 14:16:11 -0400

    Added basic slot

--- view/event_creator.php
+++ view/event_creator.php
@@ -1,11 +1,11 @@
 <script type="text/javascript" src="js/event_creator.js"></script>
 
-<div class="event_task">
+<div class="edit_event_task">
 	<div class="task_left">
-		<input type="text" />
+		<input class="task_title_input" type="text" />
 	</div>
 	<div class="task_right">
-		<input type="checkbox" />
+		<input class="comments_checkbox" type="checkbox" />
 		<div class="delete_button">Delete</div>
 	</div>
 </div>
