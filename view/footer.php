a9daad6c44bc7c126857d05775343dcd00538bb6
laurelfink <laurelfink12197@gmail.com>
2014-05-06 13:50:49 -0400

    Footer Changes

--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,8 @@
 
 </div>
 </div>
+<div class="footer_div">
 <marquee behavior="scroll" direction="right" scrollamount="10" style="color: #F0F; margin-top: 500px"><p>THIS IS THE FOOTER</p></marquee>
+</div>
 </body>
 </html>
