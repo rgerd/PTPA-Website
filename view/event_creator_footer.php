2d7072166af2e023ff5fa70e74e13beb65037c9d
laurelfink <laurelfink12197@gmail.com>
2014-05-06 13:50:33 -0400

    Event Creator Footer

--- view/event_creator_footer.php
+++ view/event_creator_footer.php
@@ -1,1 +1,7 @@
-<?php //that footer though ?>
+<div class="creator_footer">
+    <input class="solid_button" type="submit" value="Cancel"/>
+    <input class="solid_button" type="submit" value="Reminders"/>
+    <input class="solid_button" type="submit" value="Delete Event"/>
+    <input class="solid_button" type="submit" value="Preview"/>
+    <input class="solid_button" type="submit" value="Save"/>
+</div>
