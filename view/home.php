1de20b0a700998f75503f7f7c8af914db31876c0
Robert Gerdisch <rgerdisch@gmail.com>
2014-05-06 13:55:58 -0400

    Made the homepage dynamic

--- view/home.php
+++ view/home.php
@@ -6,23 +6,20 @@
 
 	<br />
 	<br />
-	<div id="events_table">
-		<div class="event">
-				<div class="event_title">Event 1</div>
-				<div class="event_description">This is an event. This is an event. This is an event. This is an event. This is an event. This is an event. This is an event.</div>
-				<div class="event_share"><a class="css_button_a"><div class="css_button_div_share">Share</div></a></div>
-		</div>
-
-		<div class="event">
-			<div class="event_title">Event 2</div>
-			<div class="event_description">This is an event. This is an event. This is an event. This is an event. This is an event. This is an event. This is an event.</div>
-			<div class="event_share"><a class="css_button_a"><div class="css_button_div_share">Share</div></a></div>
-		</div>
-
-		<div class="event">
-			<div class="event_title">Event 3</div>
-			<div class="event_description">This is an event. This is an event. This is an event. This is an event. This is an event. This is an event. This is an event.</div>
-			<div class="event_share"><a class="css_button_a"><div class="css_button_div_share">Share</div></a></div>
-		</div>
-	</div>
+	<table id="events_table">
+		<tbody>
+			<?php
+				$_events = get_events_by_user($user_id);
+				foreach($_events as $event):
+			?>
+				<tr class="event">
+						<td class="event_title"><?php echo $event['title']; ?></td>
+						<td class="event_description"><?php echo $event['description']; ?></td>
+						<td class="event_share"><a class="css_button_a"><div class="css_button_div_share">Share</div></a></td>
+				</tr>
+			<?php
+				endforeach;
+			?>
+		</tbody>
+	</table>
 </div>
