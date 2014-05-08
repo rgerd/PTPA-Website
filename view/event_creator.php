02aee434d1c5112f6a78e05c3d3ab09a499342e1
Robert Gerdisch <rgerdisch@gmail.com>
2014-05-08 14:54:17 -0400

    Starting to ask for the time

--- view/event_creator.php
+++ view/event_creator.php
@@ -11,6 +11,36 @@
 	<div id="datepicker"></div>
 </fieldset>
 <input type="hidden" id="event_date_input" name="event_date" />
+<br />
+
+<!-- ALL DAY? 
+<div>
+Time: <input type="number"/> : <input type="number"/> 
+</div>
+-->
+
+<div id="event_creator_task_container">
+        <table id="event_tasks_table">
+            <thead>
+            <th class="th" id="th_desc">Description</th>
+            <th class="th" id="th_signups">Number&nbspof&nbspSignups</th>
+            <th class="th">Comments</th>
+            <th class="th" id="th_delete"> </th>
+            </thead>
+            <tbody id="table_body"></tbody>
+        </table>
+</div>
+
+<div id="event_creator_add_button">Add Task</div>
+<br />
+<input class="solid_button" type="submit" value="Save!"/>
+<input type="hidden" name="action" value="save_event"/>
+<?php include 'event_creator_footer.php'; ?>
+</form>
+
+
+
+
 
 <!--
 	<div id="event_creator_task_header">
@@ -31,21 +61,3 @@
 	</div>
 	</div>
 -->
-<div id="event_creator_task_container">
-        <table id="event_tasks_table">
-            <thead>
-            <th class="th" id="th_desc">Description</th>
-            <th class="th" id="th_signups">Number&nbspof&nbspSignups</th>
-            <th class="th">Comments</th>
-            <th class="th" id="th_delete"> </th>
-            </thead>
-            <tbody id="table_body"></tbody>
-        </table>
-</div>
-
-<div id="event_creator_add_button">Add Task</div>
-<br />
-<input class="solid_button" type="submit" value="Save!"/>
-<input type="hidden" name="action" value="save_event"/>
-<?php include 'event_creator_footer.php'; ?>
-</form>
