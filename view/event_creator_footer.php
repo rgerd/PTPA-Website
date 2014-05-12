6c10e7626a310ac2ffc3f1c7c6e3a3a62ab37434
Robert Gerdisch <rgerdisch@gmail.com>
2014-05-12 10:51:04 -0400

    Started functionality for footer buttons

--- view/event_creator_footer.php
+++ view/event_creator_footer.php
@@ -1,7 +1,11 @@
 <div class="creator_footer">
-    <input class="solid_button" type="submit" value="Cancel"/>
-    <input class="solid_button" type="submit" value="Reminders"/>
-    <input class="solid_button" type="submit" value="Delete Event"/>
-    <input class="solid_button" type="submit" value="Preview"/>
-    <input class="solid_button" type="submit" value="Save"/>
+	<div style="float:left; margin-left: 10px">
+		<a class="solid_button" href=".">Cancel</a><br />
+		<a class="solid_button" href=".?a=delete&id=">Delete Event</a>
+    </div>
+    <div style="float:right; text-align:right; margin-right: 10px">
+    	<a class="solid_button" href=".?a=reminders&id=">Reminders</a><br />
+    	<a class="solid_button" href=".?a=prev&id=">Preview</a>
+    	<input class="solid_button" type="submit" value="Save"/>
+    </div>
 </div>
