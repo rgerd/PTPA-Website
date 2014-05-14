402a8cd94197f3d5d22eefa8810f9df121f9e2d2
Laurel Fink <laurelfink12197@gmail.com>
2014-05-14 14:11:48 -0400

    More signup stuff

--- view/task_sign_up.php
+++ view/task_sign_up.php
@@ -1,3 +1,9 @@
 <form>
+	<input type="text" name="fname" placeholder="First Name"/>
+	<input type="text" name="lname" placeholder="Last Name"/>
+	<input type="text" name="email" placeholder="Email"/>
+	<input type="text" name="phone" placeholder="Phone Number"/>
+	//if comments are allowed
+	<textarea type="text" name="comments" rows="4" placeholder="Comments"></textarea>
 	
 </form>
