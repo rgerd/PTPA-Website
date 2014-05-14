c75404aca9e79dd5ed531f0fbc7ad5764949df9c
Robert Gerdisch <rgerdisch@gmail.com>
2014-05-13 20:42:17 -0400

    Making the footer buttons work

--- view/event_creator_footer.php
+++ view/event_creator_footer.php
@@ -1,11 +1,11 @@
 <div class="creator_footer">
 	<div style="float:left; margin-left: 10px">
 		<a class="solid_button" href=".">Cancel</a><br />
-		<a class="solid_button" href=".?a=delete&id=">Delete Event</a>
+		<a class="solid_button" href=".?a=delete&e=<?php echo $event_id; ?>">Delete Event</a>
     </div>
     <div style="float:right; text-align:right; margin-right: 10px">
-    	<a class="solid_button" href=".?a=reminders&id=">Reminders</a><br />
-    	<a class="solid_button" href=".?a=prev&id=">Preview</a>
+    	<a class="solid_button" href=".?a=reminders&e=<?php echo $event_id; ?>">Reminders</a><br />
+    	<a class="solid_button" href=".?a=prev&e=<?php echo $event_id; ?>">Preview</a>
     	<input class="solid_button" type="submit" value="Save"/>
     </div>
 </div>
