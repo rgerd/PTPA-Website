3ee32d2aefe975fb568d9842935b8cd1bce449a6
Robert Gerdisch <rgerdisch@gmail.com>
2014-05-15 09:11:34 -0400

    Made form DYNAMIC

--- view/task_sign_up.php
+++ view/task_sign_up.php
@@ -1,9 +1,14 @@
+<?php
+	$task = get_task($_POST['task']);
+	$comments_enabled = $task['comments'];
+?>
+
 <form>
 	<input type="text" name="fname" placeholder="First Name"/>
 	<input type="text" name="lname" placeholder="Last Name"/>
 	<input type="text" name="email" placeholder="Email"/>
 	<input type="text" name="phone" placeholder="Phone Number"/>
-	//if comments are allowed
+	<?php if($comments_enabled): ?>
 	<textarea type="text" name="comments" rows="4" placeholder="Comments"></textarea>
-	
+	<?php endif; ?>
 </form>
