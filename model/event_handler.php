0e5d328c7510e29122cce5b5785077974cf7d463
Robert Gerdisch <rgerdisch@gmail.com>
2014-05-20 10:58:01 -0400

    Added event data handler

---
+++ model/event_handler.php
@@ -0,0 +1,21 @@
+<?php
+	function save_event($data) {
+		$event_title = sanitize($data['event_title']);
+		$event_date = sanitize($data['event_date']);
+		$event_desc = sanitize($data['event_desc']);
+		$event_id = add_event($user_id, $event_title, $event_date, $event_desc);
+		$event_task_index = 0;
+		while(true) {
+			$v = 'event_task_'.$event_task_index;
+			if(isset($data[$v])) {
+				$title = sanitize($data[$v."_title"]);
+				$slots = sanitize($data[$v."_slots"]);
+				$comments = isset($data[$v."_comments"]) ? 1 : 0;
+				add_task($event_id, $event_task_index, $title, $slots, $comments);
+			} else {
+				break;
+			}
+			$event_task_index++;
+		}
+	}
+?>
