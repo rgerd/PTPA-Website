d6eaef688b41d2fc2eb62ebbae76241b022f8a91
Robert Gerdisch <rgerdisch@gmail.com>
2014-05-20 10:57:49 -0400

    Added preview footer

---
+++ view/event_preview_footer.php
@@ -0,0 +1,14 @@
+<div class="footer preview_footer">
+	<div class="footer_left">
+		<form method="post" action=".">
+			<input type="hidden" name="action" value="preview_edit"/>
+			<input class="button light_button" type="submit" name="action" value="Edit"/>
+		</form>
+    </div>
+    <div class="footer_right">
+   		<form method="post" action=".">
+   			<input type="hidden" name="action" value="preview_save"/>
+    		<input class="button light_button" type="submit" value="Save"/>
+    	</form>
+    </div>
+</div>
