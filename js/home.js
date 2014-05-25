f30c64056d528c9c4fbfdb827388843d89681429
Robert Gerdisch <rgerdisch@gmail.com>
2014-05-25 14:42:11 -0400

    Updated ZeroClipboard

--- js/home.js
+++ js/home.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-	$(".share_button").each(function(i) {
-		var clip = new ZeroClipboard($(this), {
-			moviePath: "js/ZeroClipboard/ZeroClipboard.swf"
-		});
-		clip.on("load", function(client) {
-			client.on("complete", function(client, args) {
-				alert("The link to access this event has been copied to your clipboard.");
-			});
-		});
+	var _globalConfig = {
+			swfPath: "js/ZeroClipboard/ZeroClipboard.swf",
+		  	hoverClass: "share_button_hover",
+  			activeClass: "share_button_active",
+  			forceHandCursor: true,
+	}
+	ZeroClipboard.config(_globalConfig);
+
+	var client = new ZeroClipboard($(".share_button"));
+	client.on("complete", function(client, args) {
+		alert("The link to access this event has been copied to your clipboard.");
 	});
 });
